dcbe2af81f6ff237edfc001b71d3a1e82be0f7a4
Петър Стоев <petarstoev0505@gmail.com>
2025-01-18 17:25:49 +0200

    fix: build config

--- next.config.js
+++ next.config.js
@@ -4,14 +4,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { 
-    unoptimized: true 
-  },
-  distDir: 'out',
-  experimental: {
-    appDir: true,
-  },
-  trailingSlash: true,
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
